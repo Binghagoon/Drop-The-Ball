1085dbaf47a71713b9e5e78777a9191a44d1fadf
Binghagoon <foat3376@korea.ac.kr>
2018-06-20 01:39:39 +0900

    Create maps from 1-1 to 2-4 W.I.P.

--- Assets/Scripts/GameUIManager.cs
+++ Assets/Scripts/GameUIManager.cs
@@ -7,18 +7,33 @@ public class GameUIManager : MonoBehaviour {
 
 	const string prefix = "rate: "; 
 	Text XText, YText, ZText;
+	GameObject canvas;
+	[SerializeField]
+	Image ballImg;
 
 	void Start () {
 		XText = GameObject.Find("XRateText").GetComponent<Text>();
 		YText = GameObject.Find("YRateText").GetComponent<Text>();
 		ZText = GameObject.Find("ZRateText").GetComponent<Text>();
+		canvas = GameObject.Find("Canvas");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		Vector3 ballPos = Camera.main.WorldToViewportPoint(GameObject.Find("Ball(Clone)").transform.position);
+		RectTransform CanvasRect = canvas.GetComponent<RectTransform>();
+		ballPos.x = ballPos.x * CanvasRect.sizeDelta.x - CanvasRect.sizeDelta.x * 0.5f;
+		ballPos.y = ballPos.y * CanvasRect.sizeDelta.y - CanvasRect.sizeDelta.y * 0.5f;
+		ballImg.rectTransform.anchoredPosition = ballPos;
 		
 	}
 
+	void HangObjOnScreen()
+	{
+		
+	}
+
+	
 	public void UpdateTexts(float Xrate, float Yrate, float Zrate)
 	{
 		XText.text = "X" + prefix + Xrate.ToString();
